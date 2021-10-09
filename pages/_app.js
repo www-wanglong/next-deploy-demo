b3e955c6f31fb3a7c83728797c765981953d88a2
www-wanglong <18392700041@163.com>
2021-10-09 23:08:31 +0800

    谷歌搜索主页面基本完成

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+// core styles
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
